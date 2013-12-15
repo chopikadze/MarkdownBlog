19fd8c6afccf0243ae5ac764f30a1a19916386b5
George Kravchenko <george@softumus.com>
2013-12-15 10:48:39 +0200

    404 page when page is not found

--- Softumus.Blog/App_Start/FilterConfig.cs
+++ Softumus.Blog/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Softumus.Blog
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+			filters.Add(new HandleNotFoundAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
---
+++ Softumus.Blog/App_Start/HandleNotFoundAttribute.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Softumus.Blog
+{
+	public class HandleNotFoundAttribute : ActionFilterAttribute, IExceptionFilter
+	{
+	    public void OnException(ExceptionContext filterContext)
+	    {
+	        var httpException = filterContext.Exception.GetBaseException() as HttpException;
+	        if (httpException != null && httpException.GetHttpCode() == (int)HttpStatusCode.NotFound)
+	        {
+	            filterContext.HttpContext.Response.TrySkipIisCustomErrors = true; // Prevents IIS from intercepting the error and displaying its own content.
+	            filterContext.ExceptionHandled = true;
+	            filterContext.HttpContext.Response.StatusCode = (int) HttpStatusCode.NotFound;
+				filterContext.Result = new ViewResult 
+				{
+					ViewName = "_404",
+					ViewData = filterContext.Controller.ViewData,
+					TempData = filterContext.Controller.TempData
+				};
+	        }
+	    }
+	}
+}
