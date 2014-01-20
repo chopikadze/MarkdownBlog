d3a9906cf3396ea53ab7fafe101985826d44866b
George Kravchenko <george.kravchenko@sofistar.ch>
2014-01-20 13:31:39 +0200

    Rename Softumus.Blog to mdBlog

---
+++ mdBlog/Content/20111029/On.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Softumus.Blog.NotifyPropertyChanged
+{
+	public static class On
+	{
+		public static void PropertyChanged<T>(PropertyChangedEventArgs e, Expression<Func<T>> propertyExpression, Action action)
+		{
+			var expression = (propertyExpression.Body is UnaryExpression)
+									? (MemberExpression)((UnaryExpression)propertyExpression.Body).Operand
+									: (MemberExpression)propertyExpression.Body;
+
+			var name = expression.Member.Name;
+
+			if (e.PropertyName == name)
+				action();
+		}
+
+		public static void PropertyChanged<TSource>(PropertyChangedEventArgs e, Expression<Func<TSource, object>> propertyExpression, Action action)
+		{
+			var expression = (propertyExpression.Body is UnaryExpression)
+									? (MemberExpression)((UnaryExpression)propertyExpression.Body).Operand
+									: (MemberExpression)propertyExpression.Body;
+
+			var name = expression.Member.Name;
+
+			if (e.PropertyName == name)
+				action();
+		}
+	}
+}
---
+++ mdBlog/pages/About.md
@@ -0,0 +1,5 @@
+
+[![](static/google.png)](http://twitter.com/chopikadze)
+[![](static/twitter.png)](http://twitter.com/chopikadze)
+[![](static/linkedin.png)](http://twitter.com/chopikadze)
+[![](static/stackoverflow.png)](http://twitter.com/chopikadze)
