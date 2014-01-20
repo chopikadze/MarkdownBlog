6a6a6351d014352ce1bc22ecfb6633c816e08e8f
George Kravchenko <george.kravchenko@sofistar.ch>
2014-01-20 14:03:45 +0200

    Resolves a problem with missing assemblies

--- mdBlog/App_Start/HandleNotFoundAttribute.cs
+++ mdBlog/App_Start/HandleNotFoundAttribute.cs
@@ -2,7 +2,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Softumus.Blog
+namespace Softumus.MdBlog.App_Start
 {
 	public class HandleNotFoundAttribute : ActionFilterAttribute, IExceptionFilter
 	{
